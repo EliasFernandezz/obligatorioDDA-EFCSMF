e1d6d3f433abaafd8a6687b66fd080f2312bb375
Elias <fernandezvillaelias41@gmail.com>
2025-11-06 11:14:15 -0300

    primer commit para subir al repositorio remoto

---
+++ src/main/java/com/obligatorio/DDA/DdaApplication.java
@@ -0,0 +1,13 @@
+package com.obligatorio.DDA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obligatorio/DDA/DdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obligatorio.DDA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
