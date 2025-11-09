4235b5e04db2f8a9128e14f0476c87052ddc590d
Elias <fernandezvillaelias41@gmail.com>
2025-11-09 18:30:57 -0300

    se hizo el diseño de la pagina de menu

---
+++ src/main/java/com/obligatorio/DDA/ControladorMenu.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.obligatorio.DDA;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Usuario
+ */
+@Controller
+public class ControladorMenu {
+    @GetMapping("/")
+    public String comienzo(Model modelo) {
+
+        return "menu";
+    }
+}
